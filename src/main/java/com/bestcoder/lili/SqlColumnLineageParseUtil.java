5f3dff775133b21d833a57744e40dc2c613dab1f
yangsummer <38514467+liliBestCoder@users.noreply.github.com>
2023-05-04 10:48:22 +0800

    Update SqlColumnLineageParseUtil.java

--- src/main/java/com/bestcoder/lili/SqlColumnLineageParseUtil.java
+++ src/main/java/com/bestcoder/lili/SqlColumnLineageParseUtil.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class SqlColumnLineageParseUtil {
 
     public static Map<String, Set<String>> parse(String sql){
-       SqlColumnLineageParser sqlColumnLineageParser = new SqlColumnLineageParser(sql);
+        SqlColumnLineageParser sqlColumnLineageParser = new SqlColumnLineageParser(sql);
         return sqlColumnLineageParser.parse();
     }
 }
